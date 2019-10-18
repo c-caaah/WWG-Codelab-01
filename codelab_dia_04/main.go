0c24021f6e46c4755e38292e1a939710b942d20e
Cah <carolinedacosta.c@gmail.com>
2019-10-17 23:43:35 -0300

    Solução: arrays e slices

--- codelab_dia_04/main.go
+++ codelab_dia_04/main.go
@@ -1,5 +1,36 @@
 package main
 
+import "fmt"
+
 func main() {
-	println("coloque suas resoluções nesse arquivo :D")
+	// Exercícios dia 4
+
+	fmt.Println("<< Exercício #1 >>")
+	fmt.Println("\nArray Int")
+	a := [10]int{2, 4, 6, 8, 10, 12, 14, 16, 18, 20}
+
+	fmt.Println("Resultado:", a)
+
+	/* for range
+	   por parágrafo
+	*/
+	fmt.Println("\nFor Range")
+	for v := range a {
+		fmt.Println(v)
+
+	}
+
+	fmt.Println("\n<< Exercício #2 >>")
+
+	numeros := []int{40, 75, 157}
+	soma(numeros)
+}
+
+func soma(operandos []int) {
+	soma := 0
+	for _, valorOperando := range operandos {
+		soma = soma + valorOperando
+	}
+
+	fmt.Println(soma)
 }
